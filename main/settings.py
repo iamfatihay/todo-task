e3aaea84f394603f81d92d8d3c9f76e7bcf2d7a7
Fatih <16fay61@gmail.com>
2023-10-17 16:47:16 +0200

    Add model.py

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1fwn9e)i9)(+hbxyj#i7(e*$u!2862!5=n$jaqwpaa#f0j7cra'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third parts
+
+    # my_apps
+    'todo',
 ]
 
 MIDDLEWARE = [
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    group = models.ForeignKey('TaskGroup', on_delete=models.SET_NULL, blank=True, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
